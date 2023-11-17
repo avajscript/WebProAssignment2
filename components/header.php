15467425b7849d958385c6d93d4f4da41f1e9921
Matthew Pierce <lesbianhaircut@outlook.com>
2023-11-17 18:05:32 -0500

    Added header.php and changed from html to php

---
+++ components/header.php
@@ -0,0 +1,15 @@
+<nav>
+    <h1>
+        Task Management System
+    </h1>
+    <div>
+        <h3>
+            <a href="">Home Page</a>
+        </h3>
+        <h3><a href="">Contact Page</a></h3>
+
+        <h3>
+            <a href="">Account Page</a>
+        </h3>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -5,5 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Web Programming Group Assignment 2</title>
     </head>
-    <body></body>
+    <body>
+        <?php
+        require_once './components/header.php';
+        ?>
+    </body>
 </html>
