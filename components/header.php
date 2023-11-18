49e19c2cb13bc6e36fdbe74292735a55711bfb17
miscany <lesbianhaircut@outlook.com>
2023-11-18 13:38:32 -0500

    started on login page and added scss pages

---
+++ components/header.php
@@ -0,0 +1,21 @@
+<nav class='header flex space-between align-center'>
+
+    <img src='../images/to-do-list-64.png' alt='todo' />
+    <!-- Right side of nav -->
+    <div class="flex align-center">
+        <h5 class='mar-right-32'>
+
+            <a href="./tasks.php">
+                My Tasks
+            </a>
+        </h5>
+        <div class="default-btn">
+            <h5>
+                <a href="./login.php">
+                    LOG IN
+                </a>
+            </h5>
+        </div>
+    </div>
+    <!-- End of right side of nav-->
+</nav>
--- pages/login.php
+++ pages/login.php
@@ -1,1 +1,36 @@
-<php?
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel='stylesheet' href='../styles/main/styles.css'>
+        <title>Document</title>
+    </head>
+    <body>
+        <main>
+            <?php
+            require_once "../components/header.php";
+            ?>
+            <form class='input-form'>
+                <div class="input-field">
+                    <label>
+                        <h5>
+                            Email Address *
+                        </h5>
+                        <input type="text" placeholder='johndoe@gmail.com'>
+                    </label>
+                </div>
+                
+                <div class="input-field">
+                    <label>
+                        <h5>
+                            Password *
+                        </h5>
+                        <input type="password" placeholder="password">
+                    </label>
+                </div>
+            </form>
+        </main>
+
+    </body>
+</html>
