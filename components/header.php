235e22963cb6b6328af9a5468c6ff10b8a274376
Matthew Pierce <lesbianhaircut@outlook.com>
2023-11-27 13:22:01 -0500

    Mobile design, mobile navbar

--- components/header.php
+++ components/header.php
@@ -25,6 +25,7 @@
                     My Tasks
                 </a>
             </h5>
+
             <div class="default-btn">
                 <h5>
                     <?php
@@ -49,8 +50,70 @@
 
     <!-- Mobile view -->
     <div class="mobile">
+        <div class="mobile-nav">
+            <div class="hamburger-icon">
+                <img src="../images/icons/menu.svg" alt="menu">
+            </div>
+            <div class="nav-links">
+                <h5 class = 'mar-bottom-8'>
+
+                    <a href="../index.html">
+                        Home Page
+                    </a>
+                </h5>
+                <h5 class = 'mar-bottom-8'>
+
+                    <a href="../pages/tasks.php">
+                        My Tasks
+                    </a>
+                </h5>
+                <div class="default-btn">
+                    <h5>
+                        <?php
+                        if (isset($_SESSION['email'])) {
+                            echo " <a href='../pages/logout.php'>
+                        SIGN OUT
+                </a>";
+                        } else {
+
+                            echo " <a href='../pages/login.php'>
+                        LOG IN
+                </a>";
+                        }
+                        ?>
+
+                    </h5>
+                </div>
+            </div>
+
+        </div>
 
     </div>
     <!-- End of mobile view -->
     <!-- End of right side of nav-->
+    <script>
+        let mobileVisible = false;
+        const mobileNav = document.getElementById('mobile-nav');
+        const mobileNavLinks = document.querySelector('.mobile-nav .nav-links');
+        const menuIcon = document.querySelector(".hamburger-icon");
+
+        // used to hide or show mobile nav based on visible state
+        const toggleMobileNav = () => {
+
+            // toggle visibility
+            mobileVisible = !mobileVisible;
+
+            // show the nav if it's visible
+            if(mobileVisible) {
+                mobileNavLinks.classList.add('show-nav');
+                // hide the nav if it's not visible
+            } else {
+                mobileNavLinks.classList.remove('show-nav');
+            }
+
+        }
+
+        // toggle the mobile menu on menu click
+        menuIcon.addEventListener('click', toggleMobileNav);
+    </script>
 </nav>
